88a1430ca22f26efc700ca13505942214724fed2
Srijak Chakraborty <srijak.work@gmail.com>
2024-02-03 13:58:02 +0530

    Dataset implemented

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,3 +1,26 @@
+var programming_languages = [
+    "python",
+    "javascript",
+    "mongodb",
+    "json",
+    "java",
+    "html",
+    "css",
+    "c",
+    "csharp",
+    "golang",
+    "kotlin",
+    "php",
+    "sql",
+    "ruby"
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
 function randomWord() {
     answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
